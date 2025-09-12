2768e0fd9c125acd6644d59a795ac32e6f6c3667
A <A@DESKTOP-6S87AHB>
2025-09-12 21:17:52 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.KachulinMV.Sprint0/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.KachulinMV.Sprint0
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
